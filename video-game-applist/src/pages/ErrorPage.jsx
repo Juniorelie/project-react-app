df3c3986763c3c9b5317e907e86a98ada73b7989
Kaiser_Elie <mbakopjuniorelie@gmail.com>
2024-06-21 09:00:29 +0200

    modify some issue with error page

--- video-game-applist/src/pages/ErrorPage.jsx
+++ video-game-applist/src/pages/ErrorPage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import wasted from "../assets/wasted.gif"
+import wasted from "../assets/wasted.gif";
 
 const ErrorPage = () => {
   return (
     <div className="h-[calc(100vh-72px)] flex flex-col items-center justify-center bg-gray-900 text-white">
-      <img src={wasted} width={1000} height={500}/>
+      <img src={wasted} width={500} height={300} />
       <p className="text-red-700 my-6 text-[30px] font-bold hover:scale-110 transition-all ease-in duration-200">
         Game Over !
       </p>
